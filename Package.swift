16c01f133f62210a3090dd51a3c3c7f036b0f95b
iblacksun <blacksun1017@gmail.com>
2026-06-09 11:24:50 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ import PackageDescription
 
 let package = Package(
     name: "cmark-gfm",
+    platforms: [
+        .iOS(.v16), .macOS(.v13), .watchOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
