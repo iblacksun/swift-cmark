d69337ef7a9c084ba4769dee1c488e4110a7958b
iblacksun <blacksun1017@gmail.com>
2026-06-09 13:33:23 +0800

    Update Swift tools version and platform requirements

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,7 +19,9 @@ import PackageDescription
 let package = Package(
     name: "cmark-gfm",
     platforms: [
-        .iOS(.v14), .macOS(.v13), .watchOS(.v7)
+        .macOS(.v12),
+        .iOS(.v15),
+        .watchOS(.v8),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
